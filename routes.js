d3f8ef43a0d9ffc21392089ac2eea3e0562be90f
Johan Moab <engjmoab@gmail.com>
2020-05-29 17:15:49 -0300

    Listagem e finalização do modulo de instrutores

--- routes.js
+++ routes.js
@@ -6,9 +6,7 @@ routes.get('/', function(req, res){
     return res.redirect("/instructors")
 })
 
-routes.get('/instructors', function(req, res){
-    return res.render("instructors/index")
-})
+routes.get('/instructors', instructors.index)
 
 routes.get('/instructors/create', function(req, res){
     return res.render("instructors/create")
@@ -32,4 +30,4 @@ routes.get('/members', function(req, res){
     return res.send("members")
 })
 
-module.exports = routes
+module.exports = routes // exportador das rotas armazenadas na variavel routes.
